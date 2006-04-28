90d07efb51c4280923b5b81b28f8dabaa32a91ba
Norman Maurer <norman@apache.org>
2006-04-28 13:06:54 +0000

    AMechanismn
    git-svn-id: https://svn.apache.org/repos/asf/james/jspf/trunk@408259 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/spf/mechanismn/AMechanismn.java
@@ -0,0 +1,90 @@
+/***********************************************************************
+ * Copyright (c) 1999-2006 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+
+package org.apache.spf.mechanismn;
+
+import java.util.ArrayList;
+
+import org.apache.spf.ErrorException;
+import org.apache.spf.IPAddr;
+import org.apache.spf.IPUtil;
+import org.apache.spf.MacroExpand;
+import org.apache.spf.SPF1Data;
+
+public class AMechanismn implements GenericMechanismn {
+
+    private SPF1Data spfData;
+
+    private String mechanismnPrefix;
+
+    private String host;
+
+    private int maskLength;
+
+    /**
+     * 
+     * @see org.apache.spf.mechanismn.GenericMechanismn#init(java.lang.String, java.lang.String, int)
+     */
+    public void init(String mechanismnPrefix, String host, int maskLength) {
+
+        this.mechanismnPrefix = mechanismnPrefix;
+        this.host = host;
+        this.maskLength = maskLength;
+    }
+
+    /**
+     * 
+     * @see org.apache.spf.mechanismn.GenericMechanismn#run(org.apache.spf.SPF1Data)
+     */
+    public String run(SPF1Data spfData) throws ErrorException {
+        spfData = this.spfData;
+        ArrayList addressList = new ArrayList();
+
+        // Get the right host.
+        if (host == null) {
+            host = spfData.getCurrentDomain();
+        } else {
+            try {
+                host = new MacroExpand(spfData).expandDomain(host);
+
+            } catch (Exception e) {
+                throw new ErrorException(e.getMessage());
+            }
+        }
+        // get the ipAddress
+        try {
+            IPAddr checkAddress = IPAddr.getAddress(spfData.getIpAddress(),
+                    maskLength);
+            try {
+                addressList.addAll(spfData.getDnsProbe().getARecords(host,
+                        maskLength));
+                if (IPUtil.checkAddressList(checkAddress, addressList)) {
+                    return mechanismnPrefix;
+                }
+            } catch (Exception e) {
+                // no a records just return null
+                return null;
+            }
+        } catch (Exception e) {
+            throw new ErrorException("No valid ipAddress: "
+                    + spfData.getIpAddress());
+        }
+        // No match found
+        return null;
+    }
+
+}
