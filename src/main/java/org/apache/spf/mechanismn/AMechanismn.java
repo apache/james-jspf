0960f630760c66ff64a02387a3d5b75df2524208
Norman Maurer <norman@apache.org>
2006-04-28 05:13:36 +0000

    Create a new interface which all mechanismn should implement
    git-svn-id: https://svn.apache.org/repos/asf/james/jspf/trunk@408246 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/spf/mechanismn/AMechanismn.java
@@ -0,0 +1,10 @@
+package org.apache.spf.mechanismn;
+
+public class AMechanismn implements GenericMechanismn {
+
+    public int checkMechanismn() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
