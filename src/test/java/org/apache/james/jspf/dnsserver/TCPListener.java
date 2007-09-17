c8f08b3d1c0a358dbdd15b07906f5bdaf9b5b930
Stefano Bagnara <bago@apache.org>
2007-09-17 10:02:21 +0000

    Added ASLv2 header to dnsjava/jnamed based classes.
    git-svn-id: https://svn.apache.org/repos/asf/james/jspf/trunk@576336 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/james/jspf/dnsserver/TCPListener.java
+++ src/test/java/org/apache/james/jspf/dnsserver/TCPListener.java
@@ -1,13 +1,32 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 // ATTENTION PLEASE ATTENTION PLEASE ATTENTION PLEASE ATTENTION PLEASE 
 // ATTENTION PLEASE ATTENTION PLEASE ATTENTION PLEASE ATTENTION PLEASE  
 //
 // Part of this class have been inspired and copy&pasted from the jnamed.java
-// file found in the root of the dnsjava-2.0.3 distribution file.
+// file found in the root of the dnsjava-2.0.5 distribution file.
 //
 // The Copyright for the original work is:
 // Copyright (c) 1999-2004 Brian Wellington (bwelling@xbill.org)
 // 
-// The License for the dnsjava-2.0.3 package is BSD  
+// The License for the dnsjava-2.0.5 package is BSD  
 
 package org.apache.james.jspf.dnsserver;
 
